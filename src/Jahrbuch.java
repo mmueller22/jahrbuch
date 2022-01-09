864b4d992f025a73a2836afbd12e800e859e83b3
Maximilian <maxmueller2003@web.de>
2022-01-09 20:11:32 +0100

    added constructor and getters

--- src/Jahrbuch.java
+++ src/Jahrbuch.java
@@ -1,3 +1,22 @@
 public class Jahrbuch {
-    
+    private Schueler[] schuelerliste;
+	private int groesse;
+
+    public Jahrbuch() {
+		
+	}
+	public Jahrbuch(int groesse) {
+		schuelerliste = new Schueler[groesse];
+		this.groesse = 0;
+	}
+
+    public Schueler[] getSchuelerliste() {
+		return schuelerliste;
+	}
+	public Schueler getCurrentSchueler() {
+		return schuelerliste[groesse-1];
+	}
+	public int getGroesse(){
+		return groesse;
+	}
 }
