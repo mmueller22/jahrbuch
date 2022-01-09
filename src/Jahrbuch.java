4accf7d480946b675a8272883c4fee389cc55c29
Maximilian <maxmueller2003@web.de>
2022-01-09 21:11:34 +0100

    added several methods

--- src/Jahrbuch.java
+++ src/Jahrbuch.java
@@ -19,4 +19,40 @@ public class Jahrbuch {
 	public int getGroesse(){
 		return groesse;
 	}
+
+    public void eintragen(String vorname, String nachname, String geburtstag, String spruch, int schuelernummer) {
+		Schueler newSchueler = new Schueler(vorname, nachname, geburtstag, spruch, schuelernummer);
+		schuelerliste[groesse] = newSchueler;
+		groesse++;
+	}
+	public void eintragen(String vorname, String nachname, String geburtstag, int schuelernummer) {
+		Schueler newSchueler = new Schueler(vorname, nachname, geburtstag, null, schuelernummer);
+		schuelerliste[groesse] = newSchueler;
+		groesse++;
+	}
+	
+	public void spruchAnpassen(Schueler schueler, String newSpruch) {
+		schueler.setSpruch(newSpruch);
+	}
+	
+	public String ausgeben(int schuelernummer) {
+		    return schuelerliste[linearSearch(schuelernummer)].ausgabe();
+	}
+	public boolean schuelernummerExistent(int schuelernummer) {
+        if (linearSearch(schuelernummer) != -1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+	private int linearSearch(int target) {
+		for (int i = 0; i < groesse; i++) {
+			if (schuelerliste[i].getSchuelernummer() == target) {
+				return i;
+			}
+		}
+		return -1;
+	}
+	
+	
 }
